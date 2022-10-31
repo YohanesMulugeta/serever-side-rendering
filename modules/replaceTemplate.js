bd8ba1bca4e3f731a9c12b56c9ca975b1a435fa6
Yohanes <yohanesmulugeta21@gmail.com>
2022-10-31 10:03:44 +0300

    put replacetemplate function in its own modules from the index js

---
+++ modules/replaceTemplate.js
@@ -0,0 +1,15 @@
+module.exports = function (template, product) {
+  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%ID%}/g, product.id);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+
+  if (!product.organic)
+    output = output.replace(/{%NOTORGANIC%}/g, "not-organic");
+
+  return output;
+};
