cf60678803467e7c0a385c55aac3befc2aec3b7b
Yohanes <yohanesmulugeta21@gmail.com>
2022-11-07 17:03:21 +0300

    finilise server-side rendering and routing

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const fs = require("fs");
-const http = require("http");
-const url = require("url");
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
 
-const slugify = require("slugify");
+const slugify = require('slugify');
 
-const replaceTemplate = require("./modules/replaceTemplate");
+const replaceTemplate = require('./modules/replaceTemplate');
 
 // IMPORTANT CONCEPT
 // All top level codes are excuted only at first that is at the begining
@@ -47,18 +47,18 @@ const replaceTemplate = require("./modules/replaceTemplate");
 // SERVER
 
 // TOP LEVEL READING
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
-  "utf-8"
+  'utf-8'
 );
 const tempCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
-  "utf-8"
+  'utf-8'
 );
 const tempProduct = fs.readFileSync(
   `${__dirname}/templates/template-product.html`,
-  "utf-8"
+  'utf-8'
 );
 
 // PARSING the json
@@ -76,33 +76,33 @@ const myFirstServer = http.createServer((request, response) => {
 
   // Routes
   // OVERVIEW page ==========
-  if (pathname === "/" || pathname === "/overview") {
+  if (pathname === '/' || pathname === '/overview') {
     const cardsHtml = dataObj
       .map((ele) => replaceTemplate(tempCard, ele))
-      .join("");
+      .join('');
 
     const output = tempOverview.replace(/{%PRODUCT-CARDS%}/g, cardsHtml);
 
     // header
     response.writeHead(200, {
-      "Content-type": "text/html",
+      'Content-type': 'text/html',
     });
 
     response.end(output);
 
     // PRODUCT page  ===========
-  } else if (pathname === "/product") {
+  } else if (pathname === '/product') {
     const product = dataObj[query.id];
     const output = replaceTemplate(tempProduct, product);
     response.writeHead(200, {
-      "Content-type": "text/html",
+      'Content-type': 'text/html',
     });
 
     response.end(output);
 
     // API
-  } else if (pathname === "/api") {
-    response.writeHead(200, { "Content-type": "application/json" });
+  } else if (pathname === '/api') {
+    response.writeHead(200, { 'Content-type': 'application/json' });
 
     response.end(data);
 
@@ -110,19 +110,19 @@ const myFirstServer = http.createServer((request, response) => {
   } else {
     // writing response header
     response.writeHead(404, {
-      "Content-type": "text/html",
-      "My-own-header": "this is a test",
+      'Content-type': 'text/html',
+      'My-own-header': 'this is a test',
     });
 
-    response.end("<h1>Page not found!</h1>");
+    response.end('<h1>Page not found!</h1>');
   }
 });
 
 // the first argument 8000 is the port we listen to requests, it is a sub address on a certain host
 // the second argument is host, '127.0.0.1' is localhost or current computer
 // the third argument is a call back function that will be called as soon as the server starts lisitning
-myFirstServer.listen(8000, "127.0.0.1", () => {
-  console.log("Listning to requests on port 8000");
+myFirstServer.listen(8000, '127.0.0.1', () => {
+  console.log('Listning to requests on port 8000');
 });
 
 // ROUTING
